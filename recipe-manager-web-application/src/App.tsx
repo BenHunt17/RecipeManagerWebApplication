118c0249bb65ff5ebefbf5a441e1a96ad0e90493
BenHunt17 <benlewishunt@gmail.com>
2022-02-28 22:32:03 +0000

    Created react web application project

---
+++ recipe-manager-web-application/src/App.tsx
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <Fragment>
+      <div className="App">Recipes Manager</div>
+    </Fragment>
+  );
+}
+
+export default App;
